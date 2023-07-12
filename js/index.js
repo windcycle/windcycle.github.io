264f058429d58e58335287d709652e37709fc3bd
AndresLevin05 <96187407+windcycle@users.noreply.github.com>
2023-07-12 05:50:42 -0700

    successfully commit

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ document.querySelector("#menu-btn").onclick = () => {
 window.oncscroll = () => {
     navbar.classList.remove('active');
 }
-let slides = document.querySelectorAll('.home .slides-container .slide ');
+/*let slides = document.querySelectorAll('.home .slides-container .slide ');
 let index = 0;
 
 function next(){
@@ -20,5 +20,19 @@ function prev(){
     index= (index - 1 + slides.length) % slides.length;
     slides[index].classList.add('active');
 }
+*/
+var acc = document.getElementsByClassName("accordion");
+var i;
 
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
 
